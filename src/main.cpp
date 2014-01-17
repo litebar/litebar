7ed0dd3713485cc3fe124dfc4ac674cc0e54b0a8
Jason Moore <sk33lz@gmail.com>
2014-01-16 22:02:53 -0500

    Updated Merkelhash

--- src/main.cpp
+++ src/main.cpp
@@ -2783,7 +2783,7 @@ bool InitBlockIndex() {
         printf("%s\n", hash.ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
+        assert(block.hashMerkleRoot == uint256("02c15dc2a5083a94fdc1ef510cfaa2a33d9a3fb4ad93809a4556f4b78a0201c1"));
         block.print();
         assert(hash == hashGenesisBlock);
 
